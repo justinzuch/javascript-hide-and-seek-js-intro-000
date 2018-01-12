25461b78d5a1a69fd525915c952152d9a20f9dac
Justin Zuchowski <justin.zuchowski@gmail.com>
2018-01-12 16:58:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target');
 }
 
-function increaseRankBy(n) {
-
-}
-
+function increaseRankBy(n){
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list');
+  for (let i = 0; i < upRank.length; i++) {
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n);
+    }
+  }
 
 function deepestChild(){
   var deep = document.getElementById('grand-node').querySelectorAll('div');
