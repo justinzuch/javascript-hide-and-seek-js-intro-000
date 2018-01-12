1c1e4d748648ebfe05e5fc73a6fa45f1c1b56114
Justin Zuchowski <justin.zuchowski@gmail.com>
2018-01-12 16:57:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target');
 }
+
+function increaseRankBy(n) {
+
+}
+
+
+function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div');
+  return deep[deep.length-1];
+}
